2527b68dd1994a0a9a48a13b48a7f8dfa3ada7cd
Bobby Galli <bobbyg603@gmail.com>
2021-04-20 12:14:57 -0400

    Add project files.

---
+++ ExeArgDump/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ExeArgDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guid = Guid.NewGuid();
+            var fileName = $"{guid}.log";
+            Console.WriteLine($"Writing log to {fileName}");
+            using (var outputFile = new StreamWriter(fileName))
+            {
+                foreach (string arg in args)
+                {
+                    outputFile.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
